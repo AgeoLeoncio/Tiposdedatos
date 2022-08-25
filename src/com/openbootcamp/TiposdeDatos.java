999f203295a9fe1768e7b72a4c5bd8fc8a26ad20
ageoleoncio <ageo.carhuaricra.27@unsch.edu.pe>
2022-08-25 18:00:37 -0500

    Initial commit

---
+++ src/com/openbootcamp/TiposdeDatos.java
@@ -0,0 +1,49 @@
+package com.openbootcamp;
+
+public class TiposdeDatos {
+    public static void main(String[] args) {
+        //TIPOS DE DATOS
+        //ENTEROS
+        System.out.println("TIPOS DE DATOS ENTEROS");
+        byte numero1 = 2;
+        short numero2 = 4;
+        int numEntero = 15;
+        long numeroLong = 16L;
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numEntero);
+        System.out.println(numeroLong);
+
+        //PUNTOS FLOTANTES
+        System.out.println("TIPOS DE DATOS FLOTANTES");
+        float numeroFloat = 4.8f;
+        double numeroDouble = 10.5d;
+
+        System.out.println(numeroFloat);
+        System.out.println(numeroDouble);
+
+        //CARACTER
+        System.out.println("TIPO DE DATO CARACTER");
+        char caracter = 'l';
+
+        System.out.println(caracter);
+
+        //BOOLEANOS
+        System.out.println("TIPOS DE DATOS BOOLEANOS");
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+        //CADENAS DE TEXTO
+        System.out.println("CADENAS DE TEXTO");
+        String name = "Ageo L.";
+        String surname = "CARHUARICRA QUISPE";
+
+        System.out.println(name);
+        System.out.println(surname);
+
+    }
+}
